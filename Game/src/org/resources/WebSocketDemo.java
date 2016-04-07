4f8e4054ea7a53c99de635c63d8161d940154721
Nicolas Ferro <nferro@correo.um.edu.uy>
2016-04-06 23:22:11 -0300

    Problema resuelto: cuando se pinta un punto solo le avisa a mi contrincante, no a todas las sessiones conectadas. Se resolvio tambien el problema de que si mi tiempo expira se le corta el juego a mi contrincante, no a todas las sesiones.

--- Game/src/org/resources/WebSocketDemo.java
+++ Game/src/org/resources/WebSocketDemo.java
@@ -17,15 +17,14 @@ import javax.websocket.server.ServerEndpoint;
 
 import org.json.JSONObject;
 import org.models.Game;
-import org.services.GameService;
 
 @ServerEndpoint("/gameWS")
 public class WebSocketDemo {
 
-	private static Set<Session> allSessions = Collections.synchronizedSet(new HashSet<Session>());
+	private static Set<Session> allSessions = Collections
+			.synchronizedSet(new HashSet<Session>());
 	private static Map<Session, Game> sessionGame = new HashMap<Session, Game>();
 	private static List<Game> games = new ArrayList<Game>();
-	private static GameService service = new GameService();
 	private static int counter = 0;
 
 	@OnOpen
@@ -35,7 +34,7 @@ public class WebSocketDemo {
 		if (newGame != null) {
 			newGame.setWaiting(false);
 			newGame.getSessions()[1] = userSession;
-			newGame.getListOfUsers()[1]=counter;
+			newGame.getListOfUsers()[1] = counter;
 			counter++;
 			sessionGame.put(userSession, newGame);
 			sendArrivals(newGame);
@@ -83,8 +82,9 @@ public class WebSocketDemo {
 						sendStartGame(thisGame);
 					}
 				}
-			}else if (action.equals("paintSpot")) {
-				textToSend = "{\"action\":\"paintedSpot\", \"x\":" + json.getInt("x") + ", \"y\":" + json.getInt("y")
+			} else if (action.equals("paintSpot")) {
+				textToSend = "{\"action\":\"paintedSpot\", \"x\":"
+						+ json.getInt("x") + ", \"y\":" + json.getInt("y")
 						+ ", \"userId\":" + json.getInt("userId") + "}";
 			} else if (action.equals("timeIsOver")) {
 				textToSend = "{\"action\":\"timeIsOver\"}";
@@ -92,19 +92,15 @@ public class WebSocketDemo {
 			if (textToSend != null) {
 				Game newGame = sessionGame.get(session);
 				Session otherSession = newGame.getOtherSession(session);
-				for (Session session1 : allSessions) {
-					if (!session1.equals(session)) {
-						try {
-							
-							System.out.println("Server Send: " + textToSend);
-							session1.getBasicRemote().sendText(textToSend);
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-					}
+				try {
+					otherSession.getBasicRemote().sendText(textToSend);
+				} catch (IOException e) {
+					e.printStackTrace();
 				}
 			}
+
 		}
+
 	}
 
 	@OnClose
@@ -113,13 +109,13 @@ public class WebSocketDemo {
 		Game game = findGame(userSession);
 		// MANEJAR CUANDO SE DESCONECTA Y ESTA ESPERANDO JUGADOR O ESTA JUGANDO
 		game.setRunning(false);
-		
+
 	}
-	
-	private void sendStartGame(Game game){
+
+	private void sendStartGame(Game game) {
 		try {
-			for(Session s: game.getSessions()){
-				String textToSend="{\"action\":\"startGame\"}";
+			for (Session s : game.getSessions()) {
+				String textToSend = "{\"action\":\"startGame\"}";
 				System.out.println("Server Send: " + textToSend);
 				s.getBasicRemote().sendText(textToSend);
 			}
@@ -128,45 +124,42 @@ public class WebSocketDemo {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	
-	
-	private void sendArrivals(Game game){
-		String json="";
+
+	private void sendArrivals(Game game) {
+		String json = "";
 		try {
-			for(int i=0;i<game.getSessions().length;i++){
-				json = "{\"action\":\"opponentEnterRoom\", \"userId\":" + game.getOtherUserId(game.getListOfUsers()[i]) +  "}";
+			for (int i = 0; i < game.getSessions().length; i++) {
+				json = "{\"action\":\"opponentEnterRoom\", \"userId\":"
+						+ game.getOtherUserId(game.getListOfUsers()[i]) + "}";
 				System.out.println(json);
-				game.getSessions()[i].getBasicRemote().sendText(json);	
+				game.getSessions()[i].getBasicRemote().sendText(json);
 			}
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
 	}
 
 	private void sendSpecifications(Session userSession) {
-		
-		
-		
+
 		Game newGame = findGame(userSession);
 		for (int i = 0; i < newGame.getListOfUsers().length; i++) {
 			if (newGame.getSessions()[i].equals(userSession)) {
 				try {
 					int id;
-					if(userSession.equals(newGame.getSessions()[0])){
-						id=newGame.getListOfUsers()[0];
-					}else{
-						id=newGame.getListOfUsers()[1];
+					if (userSession.equals(newGame.getSessions()[0])) {
+						id = newGame.getListOfUsers()[0];
+					} else {
+						id = newGame.getListOfUsers()[1];
 					}
-					String json = "{\"action\":\"specifications\", \"time\":" + newGame.getTime()
-					+ ", \"matrixSize\":" + newGame.getMatrix().length
-					+ ", \"userId\":" + id + "}";
+					String json = "{\"action\":\"specifications\", \"time\":"
+							+ newGame.getTime() + ", \"matrixSize\":"
+							+ newGame.getMatrix().length + ", \"userId\":" + id
+							+ "}";
 					userSession.getBasicRemote().sendText(json);
 					break;
 				} catch (IOException e) {
