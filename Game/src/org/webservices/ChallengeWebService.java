8efad88dac316fa47b89100f02649194787252c1
Nicolas Ferro <nferro@correo.um.edu.uy>
2016-06-05 16:06:20 -0300

    Web service de challenges para App.

--- Game/src/org/webservices/ChallengeWebService.java
+++ Game/src/org/webservices/ChallengeWebService.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -25,6 +26,13 @@ public class ChallengeWebService {
 		ChallengeService challengeService = ChallengeService.getInstance();
 		return challengeService.getChallengesPerUser
 				(SessionManager.getInstance().getUser(request.getSession().getId()));
+	}
+	
+	@GET
+	@Path("/byUsernameApp/{username}")
+	public List<Challenge> getChallengesPerUserApp(@PathParam("username") String username){
+		ChallengeService challengeService = ChallengeService.getInstance();
+		return challengeService.getChallengesPerUser(username);
 	}
 	
 }
